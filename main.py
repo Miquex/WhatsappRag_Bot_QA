dfaa4538f218319b1851ffac2e496b1d43ff7ed9
Mique <andrademiguel.work@outlook.com>
2026-04-12 20:28:18 -0500

    Update url in main.py

--- main.py
+++ main.py
@@ -30,6 +30,9 @@ def check_health() -> Dict[str, str]:
     """
     return {'message': "I'm alive "}
 
+import os
+
 
 if __name__ == '__main__':
-    uvicorn.run('main:app', host='0.0.0.0', port=8000, reload=True)
+    port = int(os.getenv('PORT', 8000))
+    uvicorn.run('main:app', host='0.0.0.0', port=port, reload=False)
